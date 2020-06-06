e8bbf557269e9cdfc192bba468cd7c44bcfb519b
Pradeep Kagithalagudem <gallant.pradeep@gmail.com>
2020-06-06 20:18:07 +0530

    spring-config-cloud microservice limits-service microservice

---
+++ limits-service/src/main/java/org/abc/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,17 @@
+package org.abc.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration getLimitsConfiguration() {
+        return new LimitConfiguration(configuration.getMinimum(),configuration.getMaximum());
+    }
+}
