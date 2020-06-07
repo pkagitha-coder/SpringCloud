42534f1d6117ba6c7071be076eecb8e658903d0a
Pradeep Kagithalagudem <gallant.pradeep@gmail.com>
2020-06-07 21:03:41 +0530

    Getting currency exchange value from currency-exchange service

--- currency-conversion-service/src/main/java/org/abc/microservices/currencyconversionservice/CurrencyConverterController.java
+++ currency-conversion-service/src/main/java/org/abc/microservices/currencyconversionservice/CurrencyConverterController.java
@@ -1,10 +1,14 @@
 package org.abc.microservices.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConverterController {
@@ -13,6 +17,14 @@ public class CurrencyConverterController {
     public CurrencyConversionValue convertCurrency(@PathVariable String from,
                                                    @PathVariable String to,
                                                    @PathVariable BigDecimal quantity) {
-        return new CurrencyConversionValue(from,to, BigDecimal.valueOf(65),quantity);
+        Map<String, String> urlVariables = new HashMap<>();
+        urlVariables.put("from", from);
+        urlVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionValue> exchangeValueResp = new RestTemplate().
+                getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversionValue.class, urlVariables);
+        CurrencyConversionValue body = exchangeValueResp.getBody();
+        return new CurrencyConversionValue(from, to, body.getConversionMultiple(), quantity);
     }
 }
